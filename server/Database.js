5b5f0721667ce55dcff8b7ce554b5870315d0165
manjeet <manjeetmaurya7785@gmail.com>
2026-05-21 11:20:31 +0530

    Adding New file

--- server/Database.js
+++ server/Database.js
@@ -1,15 +1,15 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  mongoose.connect('mongodb+srv://manjeetmaurya7785_db_user:f9Q4YLCbY2Y26jSX@alldata.a9zrfm3.mongodb.net/book')
-    .then(() => {
-      console.log('Database connected successfully');
-    }
-    )
-    .catch((err) => {
-      console.log('Database connection failed', err);
-    }
-    );
-}
+  const mongoUri = process.env.MONGO_URI || 'mongodb+srv://manjeetmaurya7785_db_user:f9Q4YLCbY2Y26jSX@alldata.a9zrfm3.mongodb.net/book';
+  try {
+    await mongoose.connect(mongoUri);
+    console.log('Database connected successfully');
+  } catch (err) {
+    console.error('Database connection failed', err);
+    throw err;
+  }
+};
 
 module.exports = connectDB;
